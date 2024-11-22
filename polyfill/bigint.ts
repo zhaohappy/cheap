750d0a8a6772b731021cb7d7f2eb0861de41be07
zhaohappy <2281756061@qq.com>
2024-11-22 21:14:49 +0800

    fix: 重写 bigint polyfill

--- polyfill/bigint.ts
+++ polyfill/bigint.ts
@@ -3,35 +3,89 @@ import i64Toi32 from 'common/function/i64Toi32'
 import { SELF } from 'common/util/constant'
 import * as is from 'common/util/is'
 
+function uin32ToInt64(lowWord: number, highWord: number) {
+  if (highWord >>> 31) {
+    highWord &= 0xfffff
+    if (lowWord) {
+      lowWord -= 1
+    }
+    else {
+      lowWord = 0xffffffff
+      highWord -= 1
+    }
+    lowWord = ~lowWord
+    highWord = (~highWord) & 0xfffff
+    return BigInt(-1) * static_cast<int64>(i32Toi64(lowWord, highWord))
+  }
+  else {
+    return static_cast<int64>(i32Toi64(lowWord, highWord & 0xfffff))
+  }
+}
+
+function int64ToUint32(value: number) {
+  let [lowWord, highWord] = i64Toi32(Math.abs(Number(value)))
+  if (value < 0) {
+    // 负数的补码是正数取反加 1
+    lowWord = ~lowWord
+    // number 整数位 52，高位只能有 20 位
+    highWord = (~highWord) & 0xfffff
+    if (lowWord === 0xffffffff) {
+      lowWord = 0
+      highWord += 1
+    }
+    else {
+      lowWord += 1
+    }
+    // 置符号位为 1
+    highWord |= 0x80000000
+  }
+  return [lowWord, highWord]
+}
+
+function uint64ToUint32(value: number) {
+  let [lowWord, highWord] = i64Toi32(Math.abs(Number(value)))
+  if (value < 0) {
+    // 负数的补码是正数取反加 1
+    lowWord = ~lowWord
+    // number 整数位 52，高位只能有 20 位
+    highWord = (~highWord) & 0xfffff
+    if (lowWord === 0xffffffff) {
+      lowWord = 0
+      highWord += 1
+    }
+    else {
+      lowWord += 1
+    }
+  }
+  return [lowWord, highWord]
+}
+
+// 模拟 53 位有符号整数
 function BigInt64Array(buffer: ArrayBuffer) {
   const obj = {
-    int: new Int32Array(buffer),
-    uint: new Uint32Array(buffer),
+    view: new Uint32Array(buffer),
     buffer,
     length: buffer.byteLength >>> 3,
     byteLength: buffer.byteLength,
-    byteOffset: 0
+    byteOffset: 0,
+    BYTES_PER_ELEMENT: 8
   }
   return new Proxy(obj, {
     get: function (target, p, receiver) {
       if (is.string(p) && is.number(+p)) {
         const offset = (+p) << 1
-        const lowWord = target.uint[offset]
-        const highWord = target.int[offset + 1]
-        const value = static_cast<int64>(i32Toi64(lowWord, Math.abs(highWord)))
-        return highWord >= 0 ? value : -value
+        let lowWord = target.view[offset]
+        let highWord = target.view[offset + 1]
+        return uin32ToInt64(lowWord, highWord)
       }
       return target[p]
     },
     set: function (target, p, value, receiver) {
       if (is.string(p) && is.number(+p)) {
-        let [lowWord, highWord] = i64Toi32(Math.abs(Number(value)))
-        if (value < 0) {
-          highWord = -highWord
-        }
+        const [lowWord, highWord] = int64ToUint32(value)
         const offset = (+p) << 1
-        target.uint[offset] = lowWord
-        target.int[offset + 1] = highWord
+        target.view[offset] = lowWord
+        target.view[offset + 1] = highWord
       }
       else {
         target[p] = value
@@ -41,13 +95,15 @@ function BigInt64Array(buffer: ArrayBuffer) {
   })
 }
 
+// 模拟 52 位无符号整数
 function BigUint64Array(buffer: ArrayBuffer) {
   const obj = {
     view: new Uint32Array(buffer),
     buffer,
     length: buffer.byteLength >>> 3,
     byteLength: buffer.byteLength,
-    byteOffset: 0
+    byteOffset: 0,
+    BYTES_PER_ELEMENT: 8
   }
   return new Proxy(obj, {
     get: function (target, p, receiver) {
@@ -55,16 +111,16 @@ function BigUint64Array(buffer: ArrayBuffer) {
         const offset = (+p) << 1
         const lowWord = target.view[offset]
         const highWord = target.view[offset + 1]
-        return static_cast<int64>(i32Toi64(lowWord, highWord))
+        return static_cast<int64>(i32Toi64(lowWord, highWord & 0xfffff))
       }
       return target[p]
     },
     set: function (target, p, value, receiver) {
       if (is.string(p) && is.number(+p)) {
-        let [lowWord, highWord] = i64Toi32(value)
+        let [lowWord, highWord] = uint64ToUint32(value)
         const offset = (+p) << 1
         target.view[offset] = lowWord
-        target.view[offset + 1] = highWord
+        target.view[offset + 1] = highWord & 0xfffff
       }
       else {
         target[p] = value
@@ -94,34 +150,28 @@ export default function polyfill() {
 
   if (!SELF.DataView.prototype.getBigInt64) {
     SELF.DataView.prototype.getBigInt64 = function (this: DataView, byteOffset: number, littleEndian?: boolean) {
-      let lowWord = 0, highWord = 0
-      lowWord = this.getUint32(byteOffset + (littleEndian ? 0 : 4), littleEndian)
-      highWord = this.getInt32(byteOffset + (littleEndian ? 4 : 0), littleEndian)
-      const value = static_cast<int64>(i32Toi64(lowWord, Math.abs(highWord)))
-      return highWord >= 0 ? value : -value
+      let lowWord = this.getUint32(byteOffset + (littleEndian ? 0 : 4), littleEndian)
+      let highWord = this.getUint32(byteOffset + (littleEndian ? 4 : 0), littleEndian)
+      return uin32ToInt64(lowWord, highWord)
     }
   }
   if (!SELF.DataView.prototype.setBigInt64) {
     SELF.DataView.prototype.setBigInt64 = function (this: DataView, byteOffset: number, value: bigint, littleEndian?: boolean) {
-      let [lowWord, highWord] = i64Toi32(Math.abs(Number(value)))
-      if (value < 0) {
-        highWord = -highWord
-      }
+      let [lowWord, highWord] = int64ToUint32(Number(value))
       this.setUint32(byteOffset + (littleEndian ? 0 : 4), lowWord, littleEndian)
-      this.setInt32(byteOffset + (littleEndian ? 4 : 0), highWord, littleEndian)
+      this.setUint32(byteOffset + (littleEndian ? 4 : 0), highWord, littleEndian)
     }
   }
   if (!SELF.DataView.prototype.getBigUint64) {
     SELF.DataView.prototype.getBigUint64 = function (this: DataView, byteOffset: number, littleEndian?: boolean) {
-      let lowWord = 0, highWord = 0
-      lowWord = this.getUint32(byteOffset + (littleEndian ? 0 : 4), littleEndian)
-      highWord = this.getUint32(byteOffset + (littleEndian ? 4 : 0), littleEndian)
-      return static_cast<int64>(i32Toi64(lowWord, highWord))
+      let lowWord = this.getUint32(byteOffset + (littleEndian ? 0 : 4), littleEndian)
+      let highWord = this.getUint32(byteOffset + (littleEndian ? 4 : 0), littleEndian)
+      return static_cast<int64>(i32Toi64(lowWord, highWord & 0xfffff))
     }
   }
   if (!SELF.DataView.prototype.setBigUint64) {
     SELF.DataView.prototype.setBigUint64 = function (this: DataView, byteOffset: number, value: bigint, littleEndian?: boolean) {
-      const [lowWord, highWord] = i64Toi32(Number(value))
+      const [lowWord, highWord] = uint64ToUint32(Number(value))
       this.setUint32(byteOffset + (littleEndian ? 0 : 4), lowWord, littleEndian)
       this.setUint32(byteOffset + (littleEndian ? 4 : 0), highWord, littleEndian)
     }
