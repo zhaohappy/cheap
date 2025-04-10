0681a6a3d3925d867088187375d0f49583777da4
zhaohappy <2281756061@qq.com>
2025-04-10 13:39:25 +0800

    feat: WebAssemblyResource 接口的一些重要属性设置为只读

--- webassembly/compiler.ts
+++ webassembly/compiler.ts
@@ -14,17 +14,17 @@ import BufferWriter from 'common/io/BufferWriter'
 import nearestPowerOf2 from 'common/math/nearestPowerOf2'
 
 export interface WebAssemblyResource {
-  tableSize: number
-  tableAlign: number
-  dataSize: number
-  dataAlign: number
-  bssSize: number
-  initFuncs: string[]
-  module: WebAssembly.Module
+  readonly tableSize: number
+  readonly tableAlign: number
+  readonly dataSize: number
+  readonly dataAlign: number
+  readonly bssSize: number
+  readonly initFuncs: string[]
+  readonly module: WebAssembly.Module
   buffer: ArrayBuffer
   threadModule?: {
-    module: WebAssembly.Module
-    initFuncs: string[]
+    readonly module: WebAssembly.Module
+    readonly initFuncs: string[]
   }
   /**
    * 提前创建好的 worker pool
