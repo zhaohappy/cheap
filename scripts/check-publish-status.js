8bf28bd86b14d04ea0a4921b1b52927a1f57f9c6
zhaogaoxing <zhaogaoxing@baijiashilian.com>
2025-11-19 22:53:18 +0800

    fix: 转换为 mjs

--- scripts/check-publish-status.js
+++ scripts/check-publish-status.js
@@ -1,7 +1,10 @@
-const path = require('path')
-const fs = require('fs')
+import { resolve, dirname } from 'path'
+import { readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
 
-const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf8'))
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const json = JSON.parse(readFileSync(resolve(__dirname, '../package.json'), 'utf8'))
 
 if (json.dependencies) {
   const keys = Object.keys(json.dependencies)
